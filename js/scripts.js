0cc5aedd5fbd7a93ef0df492919a399cc6363221
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-20 11:22:29 -0300

    links added + scroll movement

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Rolar até a section quando clicar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
