f35a1d3e8520e80753f8afb64eac85cfaad1b84e
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-21 15:39:32 -0300

    media query 577~768

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+const sideMenuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+
 // Rolar até a section quando clicar
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -6,13 +9,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+
+        if(window.getComputedStyle(document.querySelector('.on-mobile')).display == 'block'){
+            toggleMenu();
+        }
     });
 });
 
-const sideMenuToggle = document.getElementById('menu-toggle');
-const sideMenu = document.getElementById('side-menu');
-
+// Abrir e fechar o menu lateral
 sideMenuToggle.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
     sideMenu.classList.toggle('open');
     sideMenuToggle.classList.toggle('open');
-});
+}
